bbdabab69eb93ab19581aeb6b557b052b8a95260
LCottin <cottin_leonard@icloud.com>
2021-04-11 12:20:37 +0200

    Added fuction move

--- Piece.cpp
+++ Piece.cpp
@@ -1,9 +1,23 @@
 #include "Piece.hpp"
 
-
-
 Piece::Piece()
 {
+    _X          = 0;
+    _Y          = 0;
+    _Type       = PAWN;
+    _Color      = false;
+    _IsSelected = false;
+} 
+
+/**
+ * Change the position of the piece
+ * @param x New X
+ * @param y New Y
+ */
+void Piece::move(int x, int y)
+{
+    _X = x;
+    _Y = y;
 }
 
 Piece::~Piece()
--- Piece.hpp
+++ Piece.hpp
@@ -4,14 +4,17 @@
 class Piece
     {
     private:
+        enum _TYPE {PAWN, BISHOP, KNIGHT, ROOK, QUEEN, KING};
         int _X;
         int _Y;
-        int _Type;          //Type of piece (fool, king ...)
+        enum _TYPE _Type;          //Type of piece (fool, king ...)
         bool _Color;        //Color (True = Black, False = white)
         bool _IsSelected; 
 
     public:
         Piece();
+        void move(int x, int y);
+        bool isMoveValid(int x, int y);
         ~Piece();
 };
 
