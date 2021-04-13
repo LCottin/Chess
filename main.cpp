c5972fbc3a6c34317ce7df3dae84d7acb98e4142
LCottin <cottin_leonard@icloud.com>
2021-04-13 17:28:51 +0200

    removed previous code

--- main.cpp
+++ main.cpp
@@ -11,26 +11,6 @@
 
 int main(int argc, char* const argv[])
 {
-    Game* game = new Game("Thomas", "Léonard");
-    delete game;
-
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
+    
     return 0;
 }
