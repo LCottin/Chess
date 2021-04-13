d134512b12f611eb646d4829082bbcfa52f7c3b7
LCottin <cottin_leonard@icloud.com>
2021-04-13 16:33:36 +0200

    All the piece on the board

---
+++ TYPES.hpp
@@ -0,0 +1,15 @@
+#ifndef __TYPES__
+#define __TYPES__
+
+enum _TYPE 
+{
+    B_PAWN = -6, B_BISHOP = -5, B_KNIGHT = -4, 
+    B_ROOK = -3, B_QUEEN = -2, B_KING = -1,
+
+    EMPTY = 0,
+
+    W_PAWN = 1, W_BISHOP = 2, W_KNIGHT = 3, 
+    W_ROOK = 4, W_QUEEN = 5, W_KING = 6
+};
+
+#endif
