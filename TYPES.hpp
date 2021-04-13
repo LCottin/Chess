ecc30fd99cf5b0f1ffa8f8d8cd5bed28739eb916
LCottin <cottin_leonard@icloud.com>
2021-04-13 17:29:50 +0200

    Removed lines jumps

--- TYPES.hpp
+++ TYPES.hpp
@@ -1,15 +1,13 @@
 #ifndef __TYPES__
 #define __TYPES__
 
-enum _TYPE 
+enum TYPES 
 {
-    B_PAWN = -6, B_BISHOP = -5, B_KNIGHT = -4, 
-    B_ROOK = -3, B_QUEEN = -2, B_KING = -1,
+    B_PAWN = -6, B_BISHOP = -5, B_KNIGHT = -4, B_ROOK = -3, B_QUEEN = -2, B_KING = -1,
 
     EMPTY = 0,
 
-    W_PAWN = 1, W_BISHOP = 2, W_KNIGHT = 3, 
-    W_ROOK = 4, W_QUEEN = 5, W_KING = 6
+    W_PAWN = 1, W_BISHOP = 2, W_KNIGHT = 3, W_ROOK = 4, W_QUEEN = 5, W_KING = 6
 };
 
 #endif
