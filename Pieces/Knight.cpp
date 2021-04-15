34698336fb17f4f66b376888d7dcb98cd4c22252
LCottin <cottin_leonard@icloud.com>
2021-04-15 17:51:02 +0200

    Created Knight

---
+++ Pieces/Knight.cpp
@@ -0,0 +1,34 @@
+#include "Knight.hpp"
+
+Knight::Knight(const bool white) : Piece(white)
+{
+    if (_IsWhite) _Type = W_KNIGHT;
+    else          _Type = B_KNIGHT;
+}
+
+/**
+ * Indicates if moving to the given position is possible
+ * @param x,y possible position
+ * @returns true if the move is possible, else false
+ */
+bool Knight::isMoveValid(const int x, const int y) const
+{
+    int dx = abs(_X - x);
+    int dy = abs(_Y - y);
+
+    //if the piece doesn't move
+    if (dx == 0 && dy == 0) return true;
+    
+    //knight can move one tile horizontally and two tiles vertically
+    if (dx + dy != 3)   return false;
+    if (dx > 2)         return false;
+    if (dy > 2)         return false;
+
+    //default
+    return true;
+}
+
+Knight::~Knight()
+{
+    
+}
---
+++ Pieces/Knight.hpp
@@ -0,0 +1,19 @@
+#ifndef __KNIGHT__
+#define __KNIGHT__
+
+#include "../Piece.hpp"
+#include "../TYPES.hpp"
+#include <math.h>
+
+class Knight : public Piece
+{
+    private:
+
+    public:
+        Knight(const bool white);
+        bool isMoveValid(const int x, const int y) const;
+        ~Knight();
+};
+
+
+#endif
