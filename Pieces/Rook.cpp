18220bd18c1af439f5bdc7f62d30508c84d063cf
LCottin <cottin_leonard@icloud.com>
2021-04-15 14:52:00 +0200

    Created rook

--- Pieces/Rook.cpp
+++ Pieces/Rook.cpp
@@ -1,1 +1,30 @@
 #include "Rook.hpp"
+
+
+Rook::Rook(const bool white) : Piece(white)
+{
+    if (_IsWhite) _Type = W_ROOK;
+    else          _Type = B_ROOK;
+}
+
+/**
+ * Indicates if moving to the given position is possible
+ * @param x,y possible position
+ * @returns true if the move is possible, else false
+ */
+bool Rook::isMoveValid(const int x, const int y) const
+{
+    int dx = abs(_X - x);
+    int dy = abs(_Y - y);
+
+    //only x or y should change, not both
+    if (dx != 0 && dy != 0) return false;
+
+    //default
+    return true;
+}
+
+Rook::~Rook()
+{
+    
+}
---
+++ Pieces/Rook.hpp
@@ -0,0 +1,19 @@
+#ifndef __ROOK__
+#define __ROOK__
+
+#include "../Piece.hpp"
+#include "../TYPES.hpp"
+#include <math.h>
+
+class Rook : public Piece
+{
+    private:
+
+    public:
+        Rook(const bool white);
+        bool isMoveValid(const int x, const int y) const;
+        ~Rook();
+};
+
+
+#endif
