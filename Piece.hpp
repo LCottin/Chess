f77ae1d8dc87646a2cf87eef7a9c22992708a56d
LCottin <cottin_leonard@icloud.com>
2021-04-17 18:31:57 +0200

    Added method and attribut to know whether the piece is alive or not

--- Piece.hpp
+++ Piece.hpp
@@ -6,18 +6,20 @@
 class Piece
 {
     protected:
-        int _X;
-        int _Y;
-        enum TYPES _Type;
-        bool _IsWhite;             //Color (True = White, False = black)
-        bool _IsSelected; 
+        int         _X;
+        int         _Y;
+        enum TYPES  _Type;
+        bool        _IsAlive;
+        bool        _IsWhite;   //Color (True = White, False = black)
+        bool        _IsSelected; 
 
     public:
         Piece(const bool white);
-        void move(const int x, const int y);
-        int getX() const;
-        int getY() const;
-        int getType() const;
+        void    move(const int x, const int y);
+        int     getX() const;
+        int     getY() const;
+        int     getType() const;
+        bool    isAlive() const;
         ~Piece();
 };
 
