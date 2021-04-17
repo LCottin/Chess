89afc6ffbe71a22ac48a54f4cb777f5bcd6daf03
LCottin <cottin_leonard@icloud.com>
2021-04-17 17:54:00 +0200

    Added beginiing of playGame method

--- Game.cpp
+++ Game.cpp
@@ -10,7 +10,45 @@ Game::Game(const string name1, const string name2)
 
     _TurnCount = 0;
     _WhiteTurn = true;
+    _Status    = INIT;
 } 
+
+/**
+ * Starts a new game
+ * MORE DETAILS ARE WELCOME
+ */
+void Game::playGame()
+{
+    //change to active status
+    _Status = ACTIVE;
+
+    while (_Status == ACTIVE)
+    {
+        /**
+         * 0 : update counter
+         * 1 : each player plays
+         * 2 : check for check
+         * 3 : check for pat (in this case it's a draw)
+         */
+
+        /* 0 : update counter */
+        _TurnCount++;
+
+        /* 1 : players play */
+        if (_WhiteTurn)
+        {
+            //_White->play();
+            _WhiteTurn = false;
+        }
+        else
+        {
+            //_Black->play();
+            _WhiteTurn = true;
+        }
+        
+    
+    }     
+}
 
 Game::~Game()
 {
