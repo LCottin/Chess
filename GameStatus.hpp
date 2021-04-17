5ae39c679177666b824cb6e54f9200c5b7c5d2b6
LCottin <cottin_leonard@icloud.com>
2021-04-17 17:54:22 +0200

    Stores every possible status of the game

---
+++ GameStatus.hpp
@@ -0,0 +1,15 @@
+#ifndef __GAMESTATUS__
+#define __GAMESTATUS__
+
+//stores status that can be encountered during the game
+enum GAMESTATUS  
+{
+    INIT,
+    DRAW,
+    ACTIVE,
+    B_WINS,
+    W_WINS,
+    CHECK
+};
+
+#endif 
