d082afcd390ebe636103d76a37b21a1610500e23
TNdu <nieddu.thomas@gmail.com>
2021-05-06 18:59:31 +0200

    Fixed Player class

--- Player.cpp
+++ Player.cpp
@@ -12,38 +12,74 @@ Player::Player(const string name, const bool white)
     _IsCheck    = false;
 
     //create and position king
-    _Pieces[0]  = new King(white);
-    _Pieces[0]->move(4, white?7:0);
+    King* king = new King(white);
+    king->move(4, white?7:0);
+    _Pieces.push_back(king);
 
     //create and position queen
-    _Pieces[1]  = new Queen(white);
-    _Pieces[1]->move(3, white?7:0);
+    Queen* queen  = new Queen(white);
+    queen->move(3, white?7:0);
+    _Pieces.push_back(queen);
+
+    /*  Did not initiate pawns this way beacause we were not sure how this behaves.
+        //create and position pawns
+        for (int i = 2; i < 10; i++)
+        {
+            Pawn* pawn = new Pawn(white);
+            pawn->move(i-2, white?6:1);
+            _Pieces.push_back(pawn);
+        }
+    */
 
     //create and position pawns
-    for (int i = 2; i < 10; i++)
-    {
-        _Pieces[i] = new Pawn(white);
-        _Pieces[i]->move(i-2, white?6:1);
-    }
-    
-    //create and position rooks
-    _Pieces[10] = new Knight(white);
-    _Pieces[10]->move(0, white?7:0);
-    _Pieces[11] = new Knight(white);
-    _Pieces[11]->move(7, white?7:0);
+    Pawn* pawn1 = new Pawn(white);
+    pawn1->move(0, white?6:1);
+    _Pieces.push_back(pawn1);
+    Pawn* pawn2 = new Pawn(white);
+    pawn2->move(1, white?6:1);
+    _Pieces.push_back(pawn2);
+    Pawn* pawn3 = new Pawn(white);
+    pawn3->move(2, white?6:1);
+    _Pieces.push_back(pawn3);
+    Pawn* pawn4 = new Pawn(white);
+    pawn4->move(3, white?6:1);
+    _Pieces.push_back(pawn4);
+    Pawn* pawn5 = new Pawn(white);
+    pawn5->move(4, white?6:1);
+    _Pieces.push_back(pawn5);
+    Pawn* pawn6 = new Pawn(white);
+    pawn6->move(5, white?6:1);
+    _Pieces.push_back(pawn6);
+    Pawn* pawn7 = new Pawn(white);
+    pawn7->move(6, white?6:1);
+    _Pieces.push_back(pawn7);
+    Pawn* pawn8 = new Pawn(white);
+    pawn8->move(7, white?6:1);
+    _Pieces.push_back(pawn8);
 
+    //create and position rooks
+    Knight* knight1 = new Knight(white);
+    knight1->move(0, white?7:0);
+    _Pieces.push_back(knight1);
+    Knight* knight2 = new Knight(white);
+    knight2->move(7, white?7:0);
+    _Pieces.push_back(knight2);
 
     //create and position knights
-    _Pieces[12] = new Rook(white);
-    _Pieces[12]->move(1, white?7:0);
-    _Pieces[13] = new Rook(white);
-    _Pieces[13]->move(6, white?7:0);
-    
+    Rook* rook1 = new Rook(white);
+    rook1->move(1, white?7:0);
+    _Pieces.push_back(rook1);
+    Rook* rook2 = new Rook(white);
+    rook2->move(6, white?7:0);
+    _Pieces.push_back(rook2);
+
     //create and position bishops
-    _Pieces[14] = new Bishop(white);
-    _Pieces[14]->move(2, white?7:0);
-    _Pieces[15] = new Bishop(white);
-    _Pieces[15]->move(5, white?7:0);
+    Bishop* bishop = new Bishop(white);
+    bishop->move(2, white?7:0);
+    _Pieces.push_back(bishop);
+    Bishop* temp = new Bishop(white);
+    temp->move(5, white?7:0);
+    _Pieces.push_back(temp);
 
     cout << "Init player " << _Pseudo << endl;
 }
@@ -86,7 +122,7 @@ void Player::setCheck(const bool isCheck)
 
 Player::~Player()
 {
-    for(int i = 0; i < _Pieces.size(); i++)
+    for(long unsigned int i = 0; i < _Pieces.size(); i++)
     {
         delete _Pieces[i];
     }
